9c18f88f7d47a2e3a67d889f735cade35fa3c2f6
Andrej Tlčina <527392@mail.muni.cz>
2023-05-11 21:44:02 +0200

    remove refetch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import "../../globals.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
